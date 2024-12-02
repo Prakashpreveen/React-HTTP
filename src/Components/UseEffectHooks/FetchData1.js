51647258f858c159d93edc2dfa0bd62256fa37f6
Divya Prakash <prakashpreveen@gmail.com>
2024-12-02 21:50:57 +0530

    added fetcing single data using button and eventlistener

--- src/Components/UseEffectHooks/FetchData1.js
+++ src/Components/UseEffectHooks/FetchData1.js
@@ -2,42 +2,40 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function FetchData1() {
-  // Assigning hooks
-  const [post, setPost] = useState(null);
-  const [id, setId] = useState(1);
-  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+  const [post, setpost] = useState(null);
+  const [id, setid] = useState(1);
+  const [buttonid, setbuttonid] = useState(1);
 
-  // Function to handle fetching data
-  const dataHandler = () => {
-    setIdFromButtonClick(id);
-  };
-
-  // Fetching data using Axios
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
-      .then((response) => setPost(response.data))
-      .catch((error) => console.log(error));
-  }, [idFromButtonClick]);
+      .get(`https://jsonplaceholder.typicode.com/posts/${buttonid}`)
+      .then((res) => setpost(res.data))
+      .catch((err) => console.log(err));
+  }, [buttonid]);
 
-  // Returning values
+  const eventHandler = (e) => {
+    if (e.key === "Enter") {
+      DataRetrived();
+    }
+  };
+
+  const DataRetrived = () => {
+    setbuttonid(id);
+    setid("");
+  };
+
   return (
-    <>
-      <h1 style={{ color: "red", background: "yellow" }}>FETCHING DATA</h1>
+    <div>
+      <h1 style={{ color: "red" }}>FETCHING THE DATA</h1>
       <input
-        type="text"
-        placeholder="Enter UserID"
+        placeholder="UserID"
         value={id}
-        onChange={(e) => setId(e.target.value)} // Update id on input change
+        onChange={(e) => setid(e.target.value)}
+        onKeyDown={eventHandler}
       />
-      <button onClick={dataHandler}>Fetch Data</button>
-      {post ? ( // Conditional rendering based on post state
-        <h3 style={{ color: "green" }}>{post.title}</h3>
-      ) : (
-        <h3 style={{ color: "green" }}>No post found or enter a valid ID</h3>
-      )}
-    </>
+      <button onClick={DataRetrived}>Fetch Data</button>
+      {post ? <h2>{post.title}</h2> : null}
+    </div>
   );
 }
-
 export default FetchData1;
